0067482ff72952b47da107f866da9e5ff760e2dc
Peter Welby <pwelby.mauz@gmail.com>
2020-04-18 14:14:56 -0400

    Start Pickup logic, add collider and Player scripts to Player prefab

---
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Base pickup class, override PickedUp(CPMPlayer) to interact
+ * on pickup
+ */
+[RequireComponent(typeof(Collider))]
+public class Pickup : MonoBehaviour
+{
+    // Used to index into player's inventory
+    // NumKinds must be last to pick up the number of categories we have
+    public enum Kind {Fuel, NumKinds};
+
+    public Kind kind;
+    Collider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider>();
+        col.isTrigger = true;
+    }
+
+    void PickedUp(Player player)
+    {
+        if (player)
+        {
+            player.inventory[(int)kind]++;
+            Debug.Log("Picked up a RefuelerPickup, new count: " + player.inventory[(int)kind]);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Pickup trigger entered");
+        Player player = other.GetComponent<Player>();
+        if (!player) return;
+        PickedUp(player);
+
+        GameObject.Destroy(this.gameObject);
+    }
+}
