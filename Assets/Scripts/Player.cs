7d64c649ae2312847e1d9dea36e2f45f9d4d106c
Peter Welby <pwelby.mauz@gmail.com>
2020-04-18 13:15:37 -0400

    Add Player script, rename CPMPlayer -> CPMMovement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int[] inventory = new int[(int)Pickup.Kind.NumKinds];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
