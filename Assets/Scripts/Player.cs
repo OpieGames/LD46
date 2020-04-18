0b873805b8859afbca291ab4c2299e812f35d8b8
smt923 <smtea923@gmail.com>
2020-04-18 20:16:21 +0100

    functionless shield toggle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int[] inventory = new int[(int)Pickup.Kind.NumKinds];
-
+    public GameObject Shield;
+    private bool shieldActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButton("Fire2"))
         {
-            Debug.Log("shield");
+            Shield.SetActive(true);
         }
+        else
+        {
+            Shield.SetActive(false);
+        }
+
+
     }
 }
