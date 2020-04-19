e57f9e4ebd0e83ccd389198c54dee88367639ca0
joshuadolman <38567422+joshuadolman@users.noreply.github.com>
2020-04-19 12:53:12 +0100

    turrets look at pizza

---
+++ Assets/Scripts/CannonLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonLook : MonoBehaviour
+{
+        public Transform HorizontalRotate;
+        public Transform VerticalRotate;
+
+        private Transform piz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        piz = GameObject.FindGameObjectWithTag("Pizza").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalRotate.LookAt(piz.position);
+    }
+}
