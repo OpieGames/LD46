6e856d46ef504615dfd457ae844d624486c6c6d1
joshuadolman <38567422+joshuadolman@users.noreply.github.com>
2020-04-19 21:35:13 +0100

    main menu, levelmanager script, adjusted jump sound, made projectiles pass through player

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NextLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadLevel(int index)
     {
-        
+        SceneManager.LoadScene(index);
     }
 }
---
+++ Assets/Scripts/ProgressData.cs
@@ -0,0 +1,11 @@
+﻿public static class ProgressData
+{
+    public static bool[] LevelCompletions;
+
+    public static float MouseSensitivity = 1.0f;
+
+    public static void SetMouseSens(float val)
+    {
+
+    }
+}
