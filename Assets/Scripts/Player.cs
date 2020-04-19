328a11864dc9e6b51766e2616d88275043e734df
smt923 <smtea923@gmail.com>
2020-04-19 14:27:30 +0100

    parry timer/stamina

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,24 +6,83 @@ public class Player : MonoBehaviour
 {
     public int[] inventory = new int[(int)Pickup.Kind.NumKinds];
     public GameObject Shield;
+    public float MaxParryTime = 0.3f;
+
+    private PlayerShield playerShield;
+    private CPMMovement playerMovement;
+    private float curParryingTime = 0.0f;
+    private bool parryingActive = false;
+    private bool parryButtonReset = true;
     void Start()
     {
         Shield.SetActive(false);
+        curParryingTime = 0.0f;
+
+        playerShield = Shield.GetComponent<PlayerShield>();
+        playerMovement = gameObject.GetComponent<CPMMovement>();
     }
 
     void Update()
     {
         if (Input.GetButton("Block"))
         {
-            Shield.SetActive(true);
-            gameObject.GetComponent<CPMMovement>().maxTopSpeed = 3.0f;
+            ShieldBlock();
+        }
+        else if (Input.GetButton("Parry") && parryButtonReset)
+        {
+            if (curParryingTime <= 0.001f)
+            {
+                ShieldParry();
+                curParryingTime += 1.0f * Time.deltaTime;
+                parryingActive = true;
+            }
+            else if (parryingActive)
+            {
+                ShieldParry();
+                curParryingTime += 1.0f * Time.deltaTime;
+                if (curParryingTime >= MaxParryTime)
+                {
+                    parryingActive = false;
+                    parryButtonReset = false;
+                }
+            }
+            else
+            {
+                Debug.Log("Player tried to parry but can't!");
+                ShieldInactive();
+                parryingActive = false;
+                parryButtonReset = false;
+                curParryingTime = Mathf.Clamp(curParryingTime - 1.0f * Time.deltaTime, 0.0f, MaxParryTime);
+            }
         }
         else
         {
-            Shield.SetActive(false);
-            gameObject.GetComponent<CPMMovement>().ResetMaxTopSpeed();
+            ShieldInactive();
+            curParryingTime = Mathf.Clamp(curParryingTime - 1.0f * Time.deltaTime, 0.0f, MaxParryTime);
         }
 
+        if (Input.GetButtonUp("Parry"))
+        {
+            parryButtonReset = true;
+        }
+    }
 
+    private void ShieldInactive()
+    {
+        Shield.SetActive(false);
+        playerShield.CurrentState = ShieldState.Inactive;
+        playerMovement.ResetMaxTopSpeed();
+    }
+    private void ShieldBlock()
+    {
+        Shield.SetActive(true);
+        playerShield.CurrentState = ShieldState.Blocking;
+        playerMovement.maxTopSpeed = 3.0f;
+    }
+    private void ShieldParry()
+    {
+        Shield.SetActive(true);
+        playerShield.CurrentState = ShieldState.Parrying;
+        playerMovement.maxTopSpeed = 3.0f;
     }
 }
--- Assets/Scripts/PlayerShield.cs
+++ Assets/Scripts/PlayerShield.cs
@@ -2,41 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ShieldState
+{
+    Inactive,
+    Blocking,
+    Parrying,
+}
+
 [RequireComponent(typeof(BoxCollider))]
 public class PlayerShield : MonoBehaviour
 {
+    public ShieldState CurrentState = ShieldState.Inactive;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("shield collided with " + other.transform.name);
         BaseProjectile proj = other.GetComponent<BaseProjectile>();
         if (proj)
         {
-            if (proj.Parryable)
+            switch (CurrentState)
             {
-                Tower t = proj.TowerFiredFrom.GetComponent<Tower>();
-                GameObject go = other.gameObject;
-                Debug.LogFormat("Parry to {0}!", proj.TowerFiredFrom.name);
-
-                go.GetComponent<Rigidbody>().velocity = Vector3.zero;
-                go.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
-
-                Vector3 lookTarget = proj.TowerFiredFrom.transform.position;
-                lookTarget.y += 1.15f; // reflect back, but just a bit lower (hopefully avoid some collisions (this is dumb))
-                Vector3 relativePos = lookTarget - transform.position;
-                Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
-                go.transform.rotation = rotation;
-                proj.HasBeenParried = true;
-
-                //go.transform.LookAt(proj.TowerFiredFrom.transform);
-                go.GetComponent<Rigidbody>().AddForce(go.transform.forward * t.ProjectileSpeed * 110.0f);
-                Destroy(go, 5.0f);
-            }
-            else
-            {
-                Debug.Log("Projectile destroyed!");
-                Destroy(other.gameObject);
+                case ShieldState.Inactive:
+                    return;
+                case ShieldState.Blocking:
+                    Blocking(proj, other);
+                    break;
+                case ShieldState.Parrying:
+                    if (proj.Parryable)
+                    {
+                        Parrying(proj, other);
+                    }
+                    break;
             }
         }
+    }
+
+    private void Blocking(BaseProjectile proj, Collider other)
+    {
+        Debug.Log("Projectile destroyed!");
+        Destroy(other.gameObject);
+    }
+
+    private void Parrying(BaseProjectile proj, Collider other)
+    {
+        Tower t = proj.TowerFiredFrom.GetComponent<Tower>();
+        GameObject go = other.gameObject;
+        Debug.LogFormat("Parry to {0}!", proj.TowerFiredFrom.name);
+
+        go.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        go.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+
+        Vector3 lookTarget = proj.TowerFiredFrom.transform.position;
+        lookTarget.y += 1.15f; // reflect back, but just a bit lower (hopefully avoid some collisions (this is dumb))
+        Vector3 relativePos = lookTarget - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        go.transform.rotation = rotation;
+        proj.HasBeenParried = true;
+
+        //go.transform.LookAt(proj.TowerFiredFrom.transform);
+        go.GetComponent<Rigidbody>().AddForce(go.transform.forward * t.ProjectileSpeed * 110.0f);
+        Destroy(go, 5.0f);
     }
 }
