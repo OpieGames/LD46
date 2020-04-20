1073a8a4fa09d84a04e74c8d4f2232639205a54f
joshuadolman <38567422+joshuadolman@users.noreply.github.com>
2020-04-20 13:43:03 +0100

    Cannons rotate properly

--- Assets/Scripts/Towers/CannonLook.cs
+++ Assets/Scripts/Towers/CannonLook.cs
@@ -17,6 +17,16 @@ public class CannonLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HorizontalRotate.LookAt(piz.position);
+        float distance = (piz.position - HorizontalRotate.position).magnitude;
+        if (gameObject.GetComponent<Tower>().Range > distance)
+        {
+            HorizontalRotate.LookAt(piz);
+            HorizontalRotate.rotation = Quaternion.Euler( new Vector3(0, HorizontalRotate.rotation.eulerAngles.y, 0) );
+
+            VerticalRotate.LookAt(piz);
+            // VerticalRotate.rotation = Quaternion.Euler ( new Vector3(VerticalRotate.rotation.eulerAngles.x, 0, 0));
+            // VerticalRotate.rotation = Quaternion.Euler( new Vector3(eulerAngles.x, 0, 0) );
+        }
+        
     }
 }
