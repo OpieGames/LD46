dbfd8dc67f180eccc42307bccf888dfd2bb78ba0
smt923 <smtea923@gmail.com>
2020-04-20 20:33:31 +0100

    replay/exit on win/lose

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Button Retry;
+    public Button Exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Retry.onClick.AddListener(RetryClicked);
+        Exit.onClick.AddListener(ExitClicked);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void RetryClicked()
+    {
+        SceneManager.LoadScene("Scenes/Level_00");
+    }
+    
+    void ExitClicked()
+    {
+        Application.Quit();
+    }
+}
